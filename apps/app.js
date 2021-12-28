78fb67d7426e8e686d8fca95aea238100b7acf0b
Ramiro Huizar <huizarramiro2@gmail.com>
2021-12-28 00:16:48 -0600

    Finished JS and styling

--- apps/app.js
+++ apps/app.js
@@ -1,13 +1,21 @@
 const button = document.querySelector('#button')
+const body = document.querySelector('#body')
+const header = document.querySelector('#header')
 
 button.addEventListener('click', function(){
      console.log('clickedddd');
 
     if(button.classList.contains('night')){
         button.classList.remove('night');
+        body.classList.remove('night-body')
+        body.classList.add('body')
+        header.classList.remove('night-header')
     }
     else{
         button.classList.add('night');
+        body.classList.add('night-body')
+        body.classList.remove('body')
+        header.classList.add('night-header')
     }
 
 });
